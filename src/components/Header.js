1023b7d9ae7b59152cb26c071d8e00cb27209f9a
Brian Bycynski <high5bri@hotmail.com>
2017-07-11 08:23:51 -0400

    adding react router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Header extends Component {
-  render () {
-    return (
-      <div>Header</div>
-    )
-  }
+const Header = (props) => {
+  return (
+    <header className='top'>
+      <h1>
+        Catch of the Day
+      </h1>
+      <h3 className='tagline'><span>{props.tagline}</span></h3>
+    </header>
+  )
 }
+
+export default Header
